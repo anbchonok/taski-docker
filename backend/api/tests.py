aa00b5586215c8056a9863e7c4c18833554c8aee
Anna Ivanova <anviv2004@yandex.ru>
2025-03-23 21:21:18 +0300

    Add tests.py in unf-8 in backend/api

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,12 +10,12 @@ class TaskiAPITestCase(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """РџСЂРѕРІРµСЂРєР° РґРѕСЃС‚СѓРїРЅРѕСЃС‚Рё СЃРїРёСЃРєР° Р·Р°РґР°С‡."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи."""
+        """РџСЂРѕРІРµСЂРєР° СЃРѕР·РґР°РЅРёСЏ Р·Р°РґР°С‡Рё."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
